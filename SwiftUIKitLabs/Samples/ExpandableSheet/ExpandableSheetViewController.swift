a1ff510bcb84ef8300234c60352ba21370fb1136
cwh2626 <cwh2626@gmail.com>
2024-05-09 12:04:40 +0900

    ✨ 확장 바텀 시트 UI 구현

--- SwiftUIKitLabs/Samples/ExpandableSheet/ExpandableSheetViewController.swift
+++ SwiftUIKitLabs/Samples/ExpandableSheet/ExpandableSheetViewController.swift
@@ -6,76 +6,171 @@
 //
 
 import UIKit
+import SnapKit
 
 class ExpandableSheetViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    private let headerView = UIView()
-    private let tableView = UITableView()
-    private let stackView = UIStackView()
+    private let rootVStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .purple
+        
+        return stackView
+    }()
+    
+    private let topContentView : UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 8
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        let contentInset = UIEdgeInsets(top: 10, left: 20, bottom: 10, right: 20)
+        collectionView.contentInset = contentInset
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return collectionView
+    }()
+    
+    private let mainContentVStackView : UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.clipsToBounds = true
+        stackView.layer.cornerRadius = 16
+        stackView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        return stackView
+    }()
+    
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.showsVerticalScrollIndicator = false
+        
+        return tableView
+    }()
+    
+    private let headerView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    
+    private var constHeight: Constraint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupStackView()
-        setupHeaderView()
-        setupTableView()
+        setupAddView()
+        setupConstaints()
         
     }
 
-    private func setupStackView() {
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.alignment = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(stackView)
-
-        NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
+    private func setupAddView() {
+        view.addSubview(rootVStackView)
+        
+        rootVStackView.addArrangedSubview(topContentView)
+        rootVStackView.addArrangedSubview(mainContentVStackView)
+        
+        topContentView.addSubview(collectionView)
+        
+        mainContentVStackView.addArrangedSubview(headerView)
+        mainContentVStackView.addArrangedSubview(tableView)
+        
+        
     }
-
-    private func setupHeaderView() {
-        headerView.backgroundColor = .blue
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.addArrangedSubview(headerView)
-        headerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.2).isActive = true
-    }
-
-    private func setupTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
-        stackView.addArrangedSubview(tableView)
+    
+    private func setupConstaints() {
+        rootVStackView.snp.makeConstraints {
+            $0.verticalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        topContentView.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.2)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        headerView.snp.makeConstraints {
+            $0.height.equalTo(100)
+        }
     }
 
     // MARK: - UIScrollViewDelegate Methods
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        if scrollView.contentOffset.y > 100 {
-                self.headerView.isHidden = false
-                UIView.animate(withDuration: 0.3,
-                               animations: {
-                    self.headerView.transform = CGAffineTransform.identity.scaledBy(x: 1, y: 0.3)
-                    self.headerView.alpha = 0
-                }, completion: { _ in
-                    self.headerView.isHidden = true
-                })
-            } else {
-                self.headerView.isHidden = false
-                self.headerView.alpha = 1
-                UIView.animate(withDuration: 0.3,
-                               animations: {
-                    self.headerView.transform = .identity
-                }, completion: nil)
-            }
+        guard scrollView == tableView else { return }
+        if scrollView.contentOffset.y > 50 {
+            UIView.animate(withDuration: 0.4,
+                           delay: 0,
+                           usingSpringWithDamping: 1,  // 댐핑 비율, 낮을수록 더 많은 바운스
+                           initialSpringVelocity: 0.2,  // 시작 속도, 높을수록 빠른 움직임
+                           options: [.curveEaseIn],
+                           animations: {
+                self.topContentView.snp.remakeConstraints {
+                    $0.height.equalTo(0)
+                }
+                self.mainContentVStackView.layer.cornerRadius = 0
+                self.view.layoutIfNeeded()
+
+            }, completion: { _ in
+            })
+        } else {
+            UIView.animate(withDuration: 0.4,
+                           delay: 0,
+                           usingSpringWithDamping: 0.6,  // 댐핑 비율, 낮을수록 더 많은 바운스
+                           initialSpringVelocity: 0.2,  // 시작 속도, 높을수록 빠른 움직임
+                           options: [.curveEaseInOut],
+                           animations: {
+                self.topContentView.snp.remakeConstraints {
+                    $0.height.equalToSuperview().multipliedBy(0.2)
+                }
+                self.mainContentVStackView.layer.cornerRadius = 16
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }
     }
 
     // MARK: - UITableViewDataSource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return 40
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+}
+
+
+extension ExpandableSheetViewController:
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout
+{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = . blue
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 60, height: collectionView.frame.size.height - 20)
+
     }
 }
